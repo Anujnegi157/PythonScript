c86bad7ffd30fe5a50cf308f9da9d15968a0614d
bitz1119 <22221137+bitz1119@users.noreply.github.com>
2024-07-30 16:45:52 +0530

    Adding few input field in frontend

--- script.py
+++ script.py
@@ -1,24 +1,20 @@
 import streamlit as st
 import requests
 
-def request_demo(name, phone_number, use_case, actor_id):
+def request_demo(customer_name, phone_number,actor_id,calling_agent_name):
     # Grab the API key securely
     api_key = st.secrets["api_keys"]["bland_ai"]
     
     # Define the prompt
     prompt = f"""
     BACKGROUND INFO:
-    Your name is Bittoo Aggarwal, and you're part of the Business Development team at Geekster. Your role involves calling and qualifying inbound leads shortly after they submit an inquiry on the Geekster website. If a lead seems surprised by the prompt response, you can explain that you're an AI phone agent designed to assist them by providing information and addressing their needs.
+    Your name is {calling_agent_name}, and you're part of the Business Development team at Geekster. Your role involves calling and qualifying inbound leads shortly after they submit an inquiry on the Geekster website. If a lead seems surprised by the prompt response, you can explain that you're an AI phone agent designed to assist them by providing information and addressing their needs.
     ...
-    * Their name is {name}
-    * Their use case is {use_case}
+    * Their name is {customer_name}
     You: Would it be helpful if I sent you a brochure and more detailed information about our programs via WhatsApp?
     Them: "Some response"
     You: Thank you for your time. I'll send the details shortly, and feel free to reach out if you have any questions!
     """
-    
-    # Define the transfer phone number
-    TRANSFER_PHONE_NUMBER = "+917982884305"
     
     # Create the data for the POST request
     data = {
@@ -26,7 +22,6 @@ def request_demo(name, phone_number, use_case, actor_id):
         "task": prompt,
         "voice_id": actor_id,
         "reduce_latency": True,
-        "transfer_phone_number": TRANSFER_PHONE_NUMBER,
     }
     
     # Make the POST request to dispatch the phone call
@@ -57,14 +52,15 @@ actor_dict = {
 # Streamlit app layout
 st.title("Receive a Phone Call from VirtualVoice")
 
-name = st.text_input("What's your name?", "Saurabh")
+customer_name = st.text_input("What's your name?", "Saurabh")
 phone_number = st.text_input("What's your phone number?", "+91 ")
 
 actor = st.selectbox("Select voice agent you are expecting", list(actor_dict.keys()))
 actor_id = actor_dict[actor]
+calling_agent_name = st.text_input("Calling Agent Name?", "Anuj")
 
 if st.button("Talk to VirtualVoice"):
-    response = request_demo(name, phone_number, "student", actor_id)
+    response = request_demo(customer_name, phone_number,actor_id,calling_agent_name)
     st.write("API Response:")
     st.write(response)
 
